78ae932dd596c0cce62ed94b5714eaa1430dffa5
vsidhu50 <vsidhu50@gmail.com>
2022-12-06 22:10:16 -0500

    day6

--- AdventOfCode2022/Day6.cs
+++ AdventOfCode2022/Day6.cs
@@ -1,64 +1,44 @@
 ﻿namespace AdventOfCode2022;
 
-public class Day5
+public class Day6
 {
-    public Day5()
+    public Day6()
     {
     }
 
-    public static string PartOne()
+    public static int PartOne()
     {
-        var commands = Utilities.GetLines(Utilities.GetInputParagraphs(5)[1]);
-        var stacks = new string[] { "NCRTMZP", "DNTSBZ", "MHQRFCTG", "GRZ", "ZNRH", "FHSWPZLD", "WDZRCGH", "SJFLHWZQ", "ZQPWN" }.Select(x => new Stack<char>(x.ToCharArray())).ToList();
-        var tops = "";
+        var buffer = Utilities.GetInputLines(6)[0];
 
-        foreach (var command in commands)
+        for (int i = 3; i < buffer.Length; i++)
         {
-            var nums = command.Replace("move ", "").Replace("from ", "").Replace("to ", "").Split(" ");
-            var quant = int.Parse(nums[0]);
-            var from = int.Parse(nums[1]) - 1;
-            var to = int.Parse(nums[2]) - 1;
+            var lastFour = new HashSet<char>();
 
-            for (int i = 0; i < quant; i++)
-            {
-                var moved = stacks[from].Pop();
-                stacks[to].Push(moved);
-            }
+            for (int j = i - 3; j <= i; j++)
+                lastFour.Add(buffer[j]);
+
+            if (lastFour.Count == 4)
+                return i + 1;
         }
 
-        foreach (var stack in stacks)
-            tops += stack.Peek();
-
-        return tops;
+        return -1;
     }
 
-    public static string PartTwo()
+    public static int PartTwo()
     {
-        var commands = Utilities.GetLines(Utilities.GetInputParagraphs(5)[1]);
-        var stacks = new string[] { "NCRTMZP", "DNTSBZ", "MHQRFCTG", "GRZ", "ZNRH", "FHSWPZLD", "WDZRCGH", "SJFLHWZQ", "ZQPWN" }.Select(x => new Stack<char>(x.ToCharArray())).ToList();
-        var tops = "";
+        var buffer = Utilities.GetInputLines(6)[0];
 
-        foreach (var command in commands)
+        for (int i = 13; i < buffer.Length; i++)
         {
-            var nums = command.Replace("move ", "").Replace("from ", "").Replace("to ", "").Split(" ");
-            var quant = int.Parse(nums[0]);
-            var from = int.Parse(nums[1]) - 1;
-            var to = int.Parse(nums[2]) - 1;
+            var lastFour = new HashSet<char>();
 
-            var moved = new Stack<char>();
+            for (int j = i - 13; j <= i; j++)
+                lastFour.Add(buffer[j]);
 
-            for (int i = 0; i < quant; i++)
-                moved.Push(stacks[from].Pop());
-
-            var numMoved = moved.Count;
-
-            for (int i = 0; i < numMoved; i++)
-                stacks[to].Push(moved.Pop());
+            if (lastFour.Count == 14)
+                return i + 1;
         }
 
-        foreach (var stack in stacks)
-            tops += stack.Peek();
-
-        return tops;
+        return -1;
     }
 }
