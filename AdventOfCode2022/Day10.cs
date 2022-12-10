57889d558a6549e7ab4afac6aea7c3135dc81789
vsidhu50 <vsidhu50@gmail.com>
2022-12-10 14:03:57 -0500

    day10

---
+++ AdventOfCode2022/Day10.cs
@@ -0,0 +1,83 @@
+﻿namespace AdventOfCode2022;
+
+public class Day10
+{
+    public Day10()
+    {
+    }
+
+    public static int PartOne()
+    {
+        var commands = Utilities.GetInputLines(10);
+        var sum = 0;
+        var cycle = 1;
+        var x = 1;
+
+        foreach (var command in commands)
+        {
+            if (cycle % 40 == 20)
+                sum += cycle * x;
+
+            var instruction = command.Split(' ')[0];
+
+            if (instruction == "noop")
+                cycle++;
+            else
+            {
+                var amount = int.Parse(command.Split(' ')[1]);
+
+                cycle++;
+                if (cycle % 40 == 20)
+                    sum += cycle * x;
+
+                cycle++;
+                x += amount;
+            }
+        }
+        return sum;
+    }
+
+    public static int PartTwo()
+    {
+        var commands = Utilities.GetInputLines(10);
+        var cycle = 0;
+        var x = 1;
+        var crt = "";
+
+        foreach (var command in commands)
+        {
+            if (cycle % 40 == 0)
+                crt += "\n";
+
+            if (cycle % 40 >= x - 1 && cycle % 40 <= x + 1)
+                crt += "#";
+            else
+                crt += ".";
+
+            var instruction = command.Split(' ')[0];
+
+            if (instruction == "noop")
+                cycle++;
+            else
+            {
+                var amount = int.Parse(command.Split(' ')[1]);
+
+                cycle++;
+
+                if (cycle % 40 == 0)
+                    crt += "\n";
+
+                if (cycle % 40 >= x - 1 && cycle % 40 <= x + 1)
+                    crt += "#";
+                else
+                    crt += ".";
+
+                cycle++;
+                x += amount;
+            }
+        }
+
+        Console.WriteLine(crt);
+        return -1;
+    }
+}
