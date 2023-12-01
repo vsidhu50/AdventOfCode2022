146619eaa74c8315078249c2ef7af4b3fd924395
vsidhu50 <vsidhu50@gmail.com>
2023-12-01 14:27:25 -0500

    day 13

--- AdventOfCode2022/Day13.cs
+++ AdventOfCode2022/Day13.cs
@@ -1,83 +1,31 @@
 ﻿namespace AdventOfCode2022;
 
-public class Day10
+public class Day13
 {
-    public Day10()
+    public Day13()
     {
     }
 
     public static int PartOne()
     {
-        var commands = Utilities.GetInputLines(10);
+        var pairs = Utilities.GetInputParagraphs(13);
+        var index = 1;
         var sum = 0;
-        var cycle = 1;
-        var x = 1;
 
-        foreach (var command in commands)
+        foreach (var pair in pairs)
         {
-            if (cycle % 40 == 20)
-                sum += cycle * x;
+            var lines = Utilities.GetLines(pair);
+            var left = lines[0];
+            var right = lines[1];
 
-            var instruction = command.Split(' ')[0];
-
-            if (instruction == "noop")
-                cycle++;
-            else
-            {
-                var amount = int.Parse(command.Split(' ')[1]);
-
-                cycle++;
-                if (cycle % 40 == 20)
-                    sum += cycle * x;
-
-                cycle++;
-                x += amount;
-            }
+            index++;
         }
+
         return sum;
     }
 
     public static int PartTwo()
     {
-        var commands = Utilities.GetInputLines(10);
-        var cycle = 0;
-        var x = 1;
-        var crt = "";
-
-        foreach (var command in commands)
-        {
-            if (cycle % 40 == 0)
-                crt += "\n";
-
-            if (cycle % 40 >= x - 1 && cycle % 40 <= x + 1)
-                crt += "#";
-            else
-                crt += ".";
-
-            var instruction = command.Split(' ')[0];
-
-            if (instruction == "noop")
-                cycle++;
-            else
-            {
-                var amount = int.Parse(command.Split(' ')[1]);
-
-                cycle++;
-
-                if (cycle % 40 == 0)
-                    crt += "\n";
-
-                if (cycle % 40 >= x - 1 && cycle % 40 <= x + 1)
-                    crt += "#";
-                else
-                    crt += ".";
-
-                cycle++;
-                x += amount;
-            }
-        }
-
-        Console.WriteLine(crt);
         return -1;
     }
 }
